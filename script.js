e64ba099096bd4ba5874bf03055e7875fc210d83
Rytham13 <97788310+Rytham13@users.noreply.github.com>
2022-09-19 08:30:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var names=new Array();
 names[0]="Yaakov";
-names[1]="Rytham";
-names[2]="Jonny";
-names[3]="jack";
-names[4]="Annie";
-names[5]="Nikki";
-names[6]="John";
-names[7]="paul";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
 names[8]="laura";
 names[9]="jim";
 
